bc034e5cf49fe77f192fc2798299f5f957eeb88c
Hitasya <33087457+Hitasya@users.noreply.github.com>
2018-06-07 11:25:30 +0400

    responsive (x-axis parallax) slider complete
    fixed the `overflow-x:hidden` compatibility issue caused by slider for Chrome by applying that declaration to even the `html` element (see `index--base.css`).
    
    But this caused a problem:
    the scroll for the menu overlay wasn't  disabling, so I changed the nav.js a bit: changed the selector for `body` to `html` and went to `nav.css` file and changed the `body.noscroll` to `html.noscroll`. Scrolling disabled on Chrome and FireFox now.

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,19 @@
 function init() {
+    /*
     document.querySelector('.hamburger-wrapper').addEventListener('click', function() {
         this.classList.toggle('active');
         document.querySelector('.overlay-wrapper').classList.toggle('show');
-        document.querySelector('body').classList.toggle('disableScrollForBody');
+        document.querySelector('body').classList.toggle('noScroll');
     })
+    */ 
 
     // alternative syntax
     
-    /*
     document.querySelector('.hamburger-wrapper').onclick = function() {
         this.classList.toggle('active');
         document.querySelector('.overlay-wrapper').classList.toggle('show');
-        document.querySelector('body').classList.toggle('disableScrollForBody');
+        document.querySelector('html').classList.toggle('no-scroll'); /*changed the class-name from camelCase to kebab-case as it stopped working (set `overflow-x` to `hidden` for `html` tag due to slider issue in the base stylesheet) and also selected `html` instead of `body`*/
     }
-    */
 }
 document.addEventListener("DOMContentLoaded", init, false);
 
