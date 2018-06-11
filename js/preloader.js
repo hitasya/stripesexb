6c37c5369b135648211d09f94b92bfde881c9227
Hitasya <33087457+Hitasya@users.noreply.github.com>
2018-06-11 11:03:18 +0400

    fixed viewport issue; added projects folder
    viewport issue (being less than body width on index page) was due to the footer. fixed by setting `flex-wrap: wrap;`
    
    css files not linking in projects page. Fixed by including a `rel='stylesheet'` attribute in the `link` tag.

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    let preloader = document.querySelector('.preloader-wrapper');
+    let main = document.querySelector('.grid-container');
+    preloader.style.display = 'none';
+    main.style.display = 'grid';
+});
