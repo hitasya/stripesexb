a0f17952ee7a4fa8386609359c6e70c00bc65151
Hitasya <33087457+Hitasya@users.noreply.github.com>
2018-06-23 10:32:58 +0400

    added lightbox to projects page

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,12 @@
 // elements
 var $page = $('.page');
+var $menu = $('.menu_items');
 
 $('.menu_toggle').on('click', function(){
   $page.toggleClass('shazam');
+  $menu.toggleClass('inactive');
 });
 $('.content').on('click', function(){
   $page.removeClass('shazam');
+  $menu.toggleClass('inactive');
 });
